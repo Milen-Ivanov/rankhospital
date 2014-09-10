713762cefcaa59d0cdee05924e6bb0ec081cda7c
balazsdukai <balazs.dukai@gmail.com>
2014-09-10 21:49:18 +0200

    commenting

--- best.R
+++ best.R
@@ -1,3 +1,17 @@
+##########################################
+#  Finding the best hospital in a state
+
+
+##########################################
+# "best" takes two arguments: the 2-character abbreviated name of a state and an
+# outcome name. The function reads the "outcome-of-care-measures.csv" file and returns a character vector
+# with the name of the hospital that has the best (i.e. lowest) 30-day mortality for the specified outcome
+# in that state. The hospital name is the name provided in the "Hospital.Name" variable. The outcomes can
+# be one of “heart attack”, “heart failure”, or “pneumonia”. 
+
+
+
+
 best <- function(state, outcome) {
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         if(!state %in% data[ ,7]) {
--- rankall.R
+++ rankall.R
@@ -1,3 +1,16 @@
+################################################
+# Ranking hospitals in all states
+
+################################################
+# "rankall" takes two arguments: an outcome name (outcome) and a hospital ranking (num). 
+# The function reads the "outcome-of-care-measures.csv" file and returns a 2-column data frame
+# containing the hospital in each state that has the ranking specified in num. For example the function call
+# rankall("heart attack", "best") would return a data frame containing the names of the hospitals that
+# are the best in their respective states for 30-day heart attack death rates. 
+
+
+
+
 rankall <- function(outcome, num = "best") {
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         
